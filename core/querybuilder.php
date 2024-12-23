7c46b18f04cf7534b4cefddd43797d8c0cd6ae5b
Helo <angheloaguilera@gmail.com>
2024-12-23 18:28:44 -0400

    Cambie un poco el querybuilder, sigo aprendiendo sobre como organizarme.

--- core/querybuilder.php
+++ core/querybuilder.php
@@ -8,22 +8,24 @@ class QueryBuilder {
     private $limit = null;
     private $values = [];
     
-    public function table($table) {
+    public function table(string $table): QueryBuilder {
         $this->table = $table;
         return $this;
     }
 
-    public function select($columns = '*') {
-        $this->select = is_array($columns) ? implode(', ', $columns) : $columns;
+    public function select(array $columns = ['*']): QueryBuilder {
+        $this->select = implode(', ', $columns);
         return $this;
     }
 
-    public function where($columns, $operators) {
+    public function where(array $columns, array $operators): QueryBuilder {
+        $this->where = array_map(fn($col, $op) => "$col $op ?",array_keys($columns), $operators);
+        $this->values = [...$this->values,...array_values($columns)];
         return $this;
     }
 
-    public function orderBy($column, $direction = 'ASC') {
-        $this->orderBy[] = "$column $direction";
+    public function orderBy(string $columns, string $directions = 'ASC') {
+        $this->orderBy[] = array_map(fn($col, $dir) => "$col $dir ",$columns, $directions);
         return $this;
     }
 
@@ -32,11 +34,29 @@ class QueryBuilder {
         return $this;
     }
 
-    public function createQuery() {
+    public function join(string $table, string $first, string $operator, string $second, string $type = 'INNER'): self
+    {
+        $this->joins[] = "$type JOIN $table ON $first $operator $second";
+        return $this;
+    }
+
+    public function delete(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function create(){
+        
+    }
+
+    public function get() {
         $query = "SELECT $this->select FROM $this->table";
         
         if (!empty($this->where)) {
-            $query .= " WHERE " . implode(' AND ', $this->where);
+            $query .= " WHERE " . (is_array($this->where) ? implode(' AND ', $this->where) : $this->where);
         }
         
         if (!empty($this->orderBy)) {
@@ -52,13 +72,14 @@ class QueryBuilder {
             'values' => $this->values ?? []
         ];
     }
-
-    public function getValues() {
-        return $this->values ?? [];
-    }
 }
 
-
+$aux = new QueryBuilder();
+$query = $aux->table("Usuarios")
+            ->select("Nombre")
+            ->where(["ID" => 5],["="])
+            ->createQuery();
+print_r($query);
 
 
 
